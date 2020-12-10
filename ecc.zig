97300a8674cbc01b2283fa7ca42cc43c4bb4c3ce
Justin Moon <mail@justinmoon.com>
2020-12-09 19:08:01 -0600

    Start building field element struct

---
+++ ecc.zig
@@ -0,0 +1,59 @@
+const expect = @import("std").testing.expect;
+const std = @import("std");
+
+const Error = error {
+    BadArgs,
+    InvalidPrime,
+};
+
+
+const FieldElement = struct {
+    num: u32,
+    prime: u32,
+
+    pub fn init(num: u32, prime: u32) FieldElement {
+        // if (num >= prime) {
+        //     return Error.BadArgs;
+        // }
+        return FieldElement {
+            .num = num,
+            .prime = prime,
+        };
+    }
+
+    pub fn eq(self: FieldElement, other: FieldElement) bool {
+        return self.num == other.num and self.prime == other.prime;
+    }
+
+    pub fn add(self: FieldElement, other: FieldElement) !FieldElement {
+        if (self.prime != other.prime) {
+            return Error.InvalidPrime;
+        }
+        const num = (self.num + other.num) % self.prime;
+        return FieldElement.init(num, self.prime);
+    }
+};
+
+test "field elements" {
+    // Constructor works
+    var fe = FieldElement.init(7, 13);
+    expect(fe.num == 7);
+    expect(fe.prime == 13);
+
+    var fe2 = FieldElement.init(7, 13);
+    var fe3 = FieldElement.init(7, 19);
+
+    expect(fe.eq(fe2));
+    expect(fe2.eq(fe));
+    expect(!fe.eq(fe3));
+    expect(!fe3.eq(fe));
+
+    // add
+    const sum = fe.add(fe) catch unreachable;
+    expect(sum.eq(FieldElement.init(1, 13)));
+}
+
+
+pub fn main() !void {
+
+}
